16930c4a7eda9b2b001fc10bf4f65b107aecb5d3
vinces1979 <vinces1979@2f9f03bc-d5b7-11dd-8ca8-e3ff79f713b6>
2008-12-29 15:29:43 +0000

    prepared package for cheeseshop

--- trunk/TGScheduler/src/setup.py
+++ trunk/TGScheduler/src/setup.py
@@ -1,38 +1,33 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages                                                                                                                                           
-from turbogears.finddata import find_package_data                                                                                                                                     
 
 import os
-execfile(os.path.join("tgcombine", "release.py"))
+execfile(os.path.join("TGScheduler", "release.py"))
 
 packages=find_packages()
-package_data = find_package_data(where='tgcombine',
-    package='tgcombine')
 
 if os.path.isdir('locales'):
     packages.append('locales')
-    package_data.update(find_package_data(where='locales',
-        exclude=('*.po',), only_in_packages=False))
 
 setup(
     name="TGScheduler",
     version=version,
-
-    # uncomment the following lines if you fill them out in release.py
     description=description,
+    long_description=long_description,
     author=author,
     author_email=email,
     url=url,
     #download_url=download_url,
     license=license,
-
+    platforms=["any"],
     install_requires=[
-        "TurboGears >= 1.0.1",
+       'TurboGears2', 
     ],
     zip_safe=False,
     packages=packages,
-    package_data=package_data,
+    #package_data=package_data,
     keywords=[
-        'turbogears.widgets',
+        'turbogears.widgets', 'tg2'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -41,6 +36,5 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Framework :: TurboGears',
         'Framework :: TurboGears :: Widgets',
-    ],
-    test_suite='nose.collector',
+    ]
     )
